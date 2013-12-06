1ffb1cfed92bfd6efabd321d4d37c8ef331e9739
Raghu Betina <rbetina@gmail.com>
2013-12-06 11:47:55 -0600

    Added one-to-many between user and pictures, added model logic

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,3 +1,8 @@
 class Picture < ActiveRecord::Base
   mount_uploader :image, ImageUploader
+
+  belongs_to :user
+
+  validates :user, presence: :true
+  validates :image, presence: :true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   mount_uploader :avatar, AvatarUploader
+
+  has_many :pictures
+
+  validates :username, presence: :true, format: { with: /\A[a-zA-Z0-9]+\Z/ }
 end
