d34e143e38576a345083a5a53d80db9e33a87f3a
Raghu Betina <rbetina@gmail.com>
2013-12-06 10:44:15 -0600

    Scaffolded users and pictures

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Initialgram::Application.routes.draw do
+  resources :pictures
+
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
