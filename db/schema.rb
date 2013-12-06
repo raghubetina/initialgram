ab98bb406ecb70503c6f7b76af00b29e2064d993
Raghu Betina <rbetina@gmail.com>
2013-12-06 10:52:00 -0600

    Allowed avatar images to be uploaded for users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131206164358) do
+ActiveRecord::Schema.define(version: 20131206164449) do
 
   create_table "pictures", force: true do |t|
     t.string   "caption"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20131206164358) do
     t.string   "username"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar"
   end
 
 end
