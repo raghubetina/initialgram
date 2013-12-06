fb82238fae26c453875ede3d4135e924e3867674
Raghu Betina <rbetina@gmail.com>
2013-12-06 12:02:44 -0600

    Stubbed out self-referential association methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,23 @@ class User < ActiveRecord::Base
   has_many :pictures
 
   validates :username, presence: :true, format: { with: /\A[a-zA-Z0-9]+\Z/ }
+
+
+  def followers
+    follows_where_followee = Follow.where(:followee_id => self.id)
+
+    result = []
+    follows_where_followee.each do |follow|
+      result << follow.follower
+    end
+
+    return result
+  end
+
+  def following
+
+  end
+
+
+
 end
